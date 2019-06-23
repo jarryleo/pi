b90abb8f962136d816bbacb2c23db0e1d8a0b07c
liujiarui <liujiarui@ship56.net>
2019-06-23 17:06:51 +0800

    舵机防抖

--- src/cn/leo/pi/sensor/SettingGear.kt
+++ src/cn/leo/pi/sensor/SettingGear.kt
@@ -1,14 +1,18 @@
 package cn.leo.pi.sensor
 
 import cn.leo.pi.gpio.GPIO
+import cn.leo.pi.utils.CoroutineUtil
 import com.pi4j.io.gpio.Pin
+import kotlinx.coroutines.delay
 
 class SettingGear(pin1: Pin) {
     val gearPin = GPIO.instance.provisionSoftPwmOutputPin(pin1, 50)
     val a = 10
     val b = 2
-    fun setDirection(direction: Int) {
-        val duty = a / 180 * direction + b
+    fun setDirection(direction: Int) = CoroutineUtil.io {
+        //val duty = a / 180 * direction + b
         gearPin.pwm = direction
+        delay(40)
+        gearPin.pwm = 0
     }
 }
